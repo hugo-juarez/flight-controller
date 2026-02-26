89d1b9e93ac2ee1af1b801d70c06320b22b515f8
Hugo Juarez <hsebastianjuarezp@gmail.com>
2026-02-26 13:35:29 -0600

    chore: Added fc_types used across the project

---
+++ Config/fc_types.h
@@ -0,0 +1,33 @@
+/* ============================================================================
+* fc_types.h
+* ============================================================================
+*
+* @file    fc_types.h
+* @author  Hugo Juarez
+* @date    2026-02-26
+* @version 0.1.0
+*
+* @brief   Hold enums/structures/definitions used accross the project.
+*
+* @details
+* Holds definition of enums/structures/defintions that are common to the
+* project and it's libraries.
+*
+* @copyright Copyright (c) 2026 Hugo Juarez. Licensed under the MIT License.
+*             See LICENSE file in the root of the repository for details.
+* ========================================================================= */
+#ifndef FLIGHT_CONTROLLER_FC_TYPES_H
+#define FLIGHT_CONTROLLER_FC_TYPES_H
+
+/* =========================================================================
+* Enums
+* ========================================================================= */
+
+typedef enum
+{
+    FC_OK = 0,
+    FC_ERR,
+} FC_Status_t;
+
+
+#endif //FLIGHT_CONTROLLER_FC_TYPES_H
