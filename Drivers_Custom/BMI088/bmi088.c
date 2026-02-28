1e07480b3e7fd5810801380ec3a3726b3cc7f417
Hugo Juarez <hsebastianjuarezp@gmail.com>
2026-02-27 20:11:48 -0600

    feat: Initialize BMI088 library

---
+++ Drivers_Custom/BMI088/bmi088.c
@@ -0,0 +1,20 @@
+/* ============================================================================
+* bmi088.c
+* ============================================================================
+*
+* @file    bmi088.c
+* @author  Hugo Juarez
+* @date    2026-02-26
+* @version 0.1.0
+*
+* @brief   BMI088 IMU Sensor Firmware
+*
+* @details
+* BMI088 is the IMU that would be used inside this flight controller and
+* this library helps read the acceleration and gyroscope data from the
+* sensor.
+*
+* @copyright Copyright (c) 2026 Hugo Juarez. Licensed under the MIT License.
+*             See LICENSE file in the root of the repository for details.
+* ========================================================================= */
+#include "bmi088.h"
---
+++ Drivers_Custom/BMI088/bmi088.h
@@ -0,0 +1,25 @@
+/* ============================================================================
+* bmi088.h
+* ============================================================================
+*
+* @file    bmi088.h
+* @author  Hugo Juarez
+* @date    2026-02-27
+* @version 0.1.0
+*
+* @brief   BMI088 IMU Sensor Firmware
+*
+* @details
+* BMI088 is the IMU that would be used inside this flight controller and
+* this library helps read the acceleration and gyroscope data from the
+* sensor.
+*
+* @copyright Copyright (c) 2026 Hugo Juarez. Licensed under the MIT License.
+*             See LICENSE file in the root of the repository for details.
+* ========================================================================= */
+#ifndef FLIGHT_CONTROLLER_BMI088_H
+#define FLIGHT_CONTROLLER_BMI088_H
+
+
+
+#endif //FLIGHT_CONTROLLER_BMI088_H
