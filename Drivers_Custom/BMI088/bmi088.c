1113e67a90ceb034516e92646859d0b526efeec9
Hugo Juarez <hsebastianjuarezp@gmail.com>
2026-03-01 00:26:29 -0600

    fix: Moved return status after CSB pin is turn high again to avoid keep reading after error

--- Drivers_Custom/BMI088/bmi088.c
+++ Drivers_Custom/BMI088/bmi088.c
@@ -46,6 +46,7 @@ FC_Status_t BMI088_Init(const BMI088_t *bmi088)
     uint8_t pwr_ctrl = 0x04;
     if ( BMI088_Accel_WriteRegister(bmi088, BMI088_ACCEL_PWR_CTRL, pwr_ctrl) != FC_OK) return FC_SPI_ERR;
 
+    // Wait for 450 microseconds
     HAL_Delay(1);
 
     if ( BMI088_WhoAmI(bmi088) != FC_OK) return FC_ERR;
@@ -56,7 +57,7 @@ FC_Status_t BMI088_Init(const BMI088_t *bmi088)
 FC_Status_t BMI088_WhoAmI(const BMI088_t *bmi088)
 {
     uint8_t chip_id;
-    BMI088_Accel_ReadRegister(bmi088, BMI088_ACCEL_CHIP_ID, &chip_id);
+    if ( BMI088_Accel_ReadRegister(bmi088, BMI088_ACCEL_CHIP_ID, &chip_id) != FC_OK) return FC_SPI_ERR;
 
     if (chip_id != 0x1E) return FC_ERR;
 
@@ -68,33 +69,39 @@ FC_Status_t BMI088_WhoAmI(const BMI088_t *bmi088)
 * ========================================================================= */
 static FC_Status_t BMI088_Accel_ReadRegister(const BMI088_t *bmi088, const BMI088_AccelReg_t reg, uint8_t *data)
 {
+    FC_Status_t status = FC_OK;
     // Turn CSB1 to 0 to initialize communication
     HAL_GPIO_WritePin(bmi088->bmi088_config.csb1_port, bmi088->bmi088_config.csb1_pin, GPIO_PIN_RESET);
 
-    // Bit #0 marks 1 as reading from register and 0 as writting to register
+    // Bit #7 marks 1 as reading from register and 0 as writting to register
     const uint8_t tx_buffer[3] = { (uint8_t) (reg | (1 << 7)) };
     uint8_t rx_buffer[3];
-    if ( HAL_SPI_TransmitReceive(bmi088->bmi088_config.spi, tx_buffer, rx_buffer, 3, HAL_MAX_DELAY) != HAL_OK) return FC_SPI_ERR;
+    if ( HAL_SPI_TransmitReceive(bmi088->bmi088_config.spi, tx_buffer, rx_buffer, 3, HAL_MAX_DELAY) != HAL_OK) status = FC_SPI_ERR;
 
     // Turn CSB1 to 1 to finalize communication
     HAL_GPIO_WritePin(bmi088->bmi088_config.csb1_port, bmi088->bmi088_config.csb1_pin, GPIO_PIN_SET);
 
-    *data = rx_buffer[2];
+    if (status == FC_OK)
+    {
+        *data = rx_buffer[2];
+    }
 
-    return FC_OK;
+    return status;
 }
 
 static FC_Status_t BMI088_Accel_WriteRegister(const BMI088_t *bmi088, const BMI088_AccelReg_t reg, const uint8_t data)
 {
+    FC_Status_t status = FC_OK;
+
     // Turn CSB1 to 0 to initialize communication
     HAL_GPIO_WritePin(bmi088->bmi088_config.csb1_port, bmi088->bmi088_config.csb1_pin, GPIO_PIN_RESET);
 
-    // Bit #0 marks 1 as reading from register and 0 as writing to register
+    // Bit #7 marks 1 as reading from register and 0 as writing to register
     const uint8_t tx_buffer[2] = { (uint8_t) (reg & ~(1 << 7)), data };
-    if ( HAL_SPI_Transmit(bmi088->bmi088_config.spi, tx_buffer, 2, HAL_MAX_DELAY) != HAL_OK) return FC_SPI_ERR;
+    if ( HAL_SPI_Transmit(bmi088->bmi088_config.spi, tx_buffer, 2, HAL_MAX_DELAY) != HAL_OK) status = FC_SPI_ERR;
 
     // Turn CSB1 to 1 to finalize communication
     HAL_GPIO_WritePin(bmi088->bmi088_config.csb1_port, bmi088->bmi088_config.csb1_pin, GPIO_PIN_SET);
 
-    return FC_OK;
+    return status;
 }
