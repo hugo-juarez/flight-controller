3554daa0fe0bbab730c4f5df7638970ff4b677f5
Hugo Juarez <hsebastianjuarezp@gmail.com>
2026-02-28 18:29:55 -0600

    chore: Initialize IMU task

---
+++ Tasks/Inc/task_imu.h
@@ -0,0 +1,27 @@
+/* ============================================================================
+* task_imu.h
+* ============================================================================
+*
+* @file    task_imu.h
+* @author  Hugo Juarez
+* @date    2026-02-28
+* @version 0.1.0
+*
+* @brief   Task that handles IMU data read for flight controller.
+*
+* @details
+* This task read IMU data from the BMI088 module neccessary from the flight
+* controller through SPI.
+*
+* @copyright Copyright (c) 2026 Hugo Juarez. Licensed under the MIT License.
+*             See LICENSE file in the root of the repository for details.
+* ========================================================================= */
+#ifndef FLIGHT_CONTROLLER_TASK_IMU_H
+#define FLIGHT_CONTROLLER_TASK_IMU_H
+
+/* =========================================================================
+* Public APIs
+* ========================================================================= */
+void task_imu(void *hw);
+
+#endif //FLIGHT_CONTROLLER_TASK_IMU_H
---
+++ Tasks/Src/task_imu.c
@@ -0,0 +1,27 @@
+/* ============================================================================
+* task_imu.c
+* ============================================================================
+*
+* @file    task_imu.c
+* @author  Hugo Juarez
+* @date    2026-02-28
+* @version 0.1.0
+*
+* @brief   Task that handles IMU data read for flight controller.
+*
+* @details
+* This task read IMU data from the BMI088 module neccessary from the flight
+* controller through SPI.
+*
+* @copyright Copyright (c) 2026 Hugo Juarez. Licensed under the MIT License.
+*             See LICENSE file in the root of the repository for details.
+* ========================================================================= */
+#include "task_imu.h"
+
+/* =========================================================================
+* Public APIs
+* ========================================================================= */
+void task_imu(void *hw)
+{
+    
+}
