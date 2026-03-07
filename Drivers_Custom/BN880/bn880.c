86865641f15ceb4edc31443e7b096161b18643ed
Hugo Juarez <hsebastianjuarezp@gmail.com>
2026-03-06 19:12:10 -0600

    chore: Initialized BN880 drivers

---
+++ Drivers_Custom/BN880/bn880.c
@@ -0,0 +1,18 @@
+/* ============================================================================
+* bn880.h
+* ============================================================================
+*
+* @file    bn880.h
+* @author  Hugo Juarez
+* @date    2026-02-26
+* @version 0.1.0
+*
+* @brief   This library hold the APIs for the BN880 GPS sensor.
+*
+* @details
+* This file holds the APIs to communicate and pull data form BN880 GPS sensor's
+* M8030-KT GPS and HMC5883L magnetometer. Using UBX as communication for GPS.
+*
+* @copyright Copyright (c) 2026 Hugo Juarez. Licensed under the MIT License.
+*             See LICENSE file in the root of the repository for details.
+* ========================================================================= */
