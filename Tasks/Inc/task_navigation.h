d621c3ee00c81f5198616ff8f28f455af07c07dc
Hugo Juarez <hsebastianjuarezp@gmail.com>
2026-03-06 19:04:52 -0600

    chore: Initialized navigation task

---
+++ Tasks/Inc/task_navigation.h
@@ -0,0 +1,23 @@
+/* ============================================================================
+* task_navigation.h
+* ============================================================================
+*
+* @file    task_navigation.h
+* @author  Hugo Juarez
+* @date    2026-03-06
+* @version 0.1.0
+*
+* @brief   LIbrary that contains GPS and navigation tasks.
+*
+* @details
+* This file include GPS and navigation related FreeRTOS tasks.
+*
+* @copyright Copyright (c) 2026 Hugo Juarez. Licensed under the MIT License.
+*             See LICENSE file in the root of the repository for details.
+* ========================================================================= */
+#ifndef FLIGHT_CONTROLLER_TASK_NAVIGATION_H
+#define FLIGHT_CONTROLLER_TASK_NAVIGATION_H
+
+
+
+#endif //FLIGHT_CONTROLLER_TASK_NAVIGATION_H
---
+++ Tasks/Src/task_navigation.c
@@ -0,0 +1,17 @@
+/* ============================================================================
+* task_navigation.c
+* ============================================================================
+*
+* @file    task_navigation.c
+* @author  Hugo Juarez
+* @date    2026-03-06
+* @version 0.1.0
+*
+* @brief   LIbrary that contains GPS and navigation tasks.
+*
+* @details
+* This file include GPS and navigation related FreeRTOS tasks.
+*
+* @copyright Copyright (c) 2026 Hugo Juarez. Licensed under the MIT License.
+*             See LICENSE file in the root of the repository for details.
+* ========================================================================= */
