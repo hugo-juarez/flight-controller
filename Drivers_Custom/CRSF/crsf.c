95ecc475c0aaad0d48fad4bffee18fd505e8bd7f
Hugo Juarez <hsebastianjuarezp@gmail.com>
2026-03-20 16:07:12 -0500

    feat: Added CRSF init function to enable DMA reads

--- Drivers_Custom/CRSF/crsf.c
+++ Drivers_Custom/CRSF/crsf.c
@@ -17,3 +17,28 @@
 *             See LICENSE file in the root of the repository for details.
 * ========================================================================= */
 #include "crsf.h"
+
+__attribute__((section(".sram2")))
+__attribute__((aligned(32)))
+static uint8_t crsf_dma[CRSF_DMA_BUF_SIZE];
+
+
+/* =========================================================================
+* Public APIs
+* ========================================================================= */
+FC_Status_t CRSF_Init(CRSF_t *crsf)
+{
+    if (crsf == NULL) return FC_NULL_PTR_ERR;
+
+    // Enable DMA reading till IDLE interrupt
+    if ( HAL_UARTEx_ReceiveToIdle_DMA(crsf->uart, crsf_dma, CRSF_DMA_BUF_SIZE) != HAL_OK)
+    {
+        return FC_UART_ERR;
+    }
+
+    // Disable DMA Half and Complete interrupts; only Idle interrupt will trigger Ex_Event Callback
+    __HAL_DMA_DISABLE_IT(crsf->uart->hdmarx, DMA_IT_HT);
+    __HAL_DMA_DISABLE_IT(crsf->uart->hdmarx, DMA_IT_TC);
+
+    return FC_OK;
+}
--- Drivers_Custom/CRSF/crsf.h
+++ Drivers_Custom/CRSF/crsf.h
@@ -16,8 +16,29 @@
 * @copyright Copyright (c) 2026 Hugo Juarez. Licensed under the MIT License.
 *             See LICENSE file in the root of the repository for details.
 * ========================================================================= */
-
 #ifndef FLIGHT_CONTROLLER_CRSF_H
 #define FLIGHT_CONTROLLER_CRSF_H
+
+#include "stm32f7xx_hal.h"
+#include "fc_types.h"
+
+/* =========================================================================
+* Macros
+* ========================================================================= */
+#define CRSF_TASK_NOTIFY_INDEX      0
+#define CRSF_DMA_BUF_SIZE           256U
+
+/* =========================================================================
+* Structs
+* ========================================================================= */
+typedef struct
+{
+    UART_HandleTypeDef *uart;
+} CRSF_t;
+
+/* =========================================================================
+* Public APIs
+* ========================================================================= */
+FC_Status_t CRSF_Init(CRSF_t *crsf);
 
 #endif //FLIGHT_CONTROLLER_CRSF_H
