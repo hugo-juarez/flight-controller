e5fd28919e41fd8efa3730afa67fa87bd12b9f82
Hugo Juarez <hsebastianjuarezp@gmail.com>
2026-03-20 14:26:54 -0500

    feat: Initialized CRSF driver

---
+++ Drivers_Custom/CRSF/crsf.c
@@ -0,0 +1,19 @@
+/* ============================================================================
+* crsf.c
+* ============================================================================
+*
+* @file    crsf.c
+* @author  Hugo Juarez
+* @date    2026-03-20
+* @version 0.1.0
+*
+* @brief   This is the CRSF protocol parsing and handler driver.
+*
+* @details
+* This driver allows to parse the crsf messages received and sent through the ELRS
+* module
+*
+* @copyright Copyright (c) 2026 Hugo Juarez. Licensed under the MIT License.
+*             See LICENSE file in the root of the repository for details.
+* ========================================================================= */
+#include "crsf.h"
---
+++ Drivers_Custom/CRSF/crsf.h
@@ -0,0 +1,23 @@
+/* ============================================================================
+* crsf.h
+* ============================================================================
+*
+* @file    crsf.h
+* @author  Hugo Juarez
+* @date    2026-03-20
+* @version 0.1.0
+*
+* @brief   This is the CRSF protocol parsing and handler driver.
+*
+* @details
+* This driver allows to parse the crsf messages received and sent through the ELRS
+* module
+*
+* @copyright Copyright (c) 2026 Hugo Juarez. Licensed under the MIT License.
+*             See LICENSE file in the root of the repository for details.
+* ========================================================================= */
+
+#ifndef FLIGHT_CONTROLLER_CRSF_H
+#define FLIGHT_CONTROLLER_CRSF_H
+
+#endif //FLIGHT_CONTROLLER_CRSF_H
