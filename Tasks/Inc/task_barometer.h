1f5460c65aa5792f4ab66fc19afe98121e70bb02
Hugo Juarez <hsebastianjuarezp@gmail.com>
2026-03-24 18:51:11 -0500

    chore: Initialized barometer task

---
+++ Tasks/Inc/task_barometer.h
@@ -0,0 +1,22 @@
+/* ============================================================================
+* task_barometer.h
+* ============================================================================
+*
+* @file    task_barometer.h
+* @author  Hugo Juarez
+* @date    2026-03-24
+* @version 0.1.0
+*
+* @brief   Task that gets the barometer data to get altitude control.
+*
+* @details
+* This task helps get altitude data from BMP390 sensor to get drone altitude
+* control.
+*
+* @copyright Copyright (c) 2026 Hugo Juarez. Licensed under the MIT License.
+*             See LICENSE file in the root of the repository for details.
+* ========================================================================= */
+#ifndef FLIGHT_CONTROLLER_TASK_BAROMETER_H
+#define FLIGHT_CONTROLLER_TASK_BAROMETER_H
+
+#endif //FLIGHT_CONTROLLER_TASK_BAROMETER_H
---
+++ Tasks/Src/task_barometer.c
@@ -0,0 +1,19 @@
+/* ============================================================================
+* task_barometer.c
+* ============================================================================
+*
+* @file    task_barometer.c
+* @author  Hugo Juarez
+* @date    2026-03-24
+* @version 0.1.0
+*
+* @brief   Task that gets the barometer data to get altitude control.
+*
+* @details
+* This task helps get altitude data from BMP390 sensor to get drone altitude
+* control.
+*
+* @copyright Copyright (c) 2026 Hugo Juarez. Licensed under the MIT License.
+*             See LICENSE file in the root of the repository for details.
+* ========================================================================= */
+#include "task_barometer.h"
