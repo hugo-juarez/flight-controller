036d6f96e3cc8712b2420610d1490e9ffc122728
Hugo Juarez <hsebastianjuarezp@gmail.com>
2026-03-24 18:34:35 -0500

    chore: Initialized BMP390 driver

---
+++ Drivers_Custom/BMP390/bmp390.c
@@ -0,0 +1,19 @@
+/* ============================================================================
+* bmp390.h
+* ============================================================================
+*
+* @file    bmp390.h
+* @author  Hugo Juarez
+* @date    2026-03-24
+* @version 0.1.0
+*
+* @brief   BMP390 driver to get barometric pressure data.
+*
+* @details
+* This driver should allow to grab data from BM390 sensor for altitude PID
+* controller.
+*
+* @copyright Copyright (c) 2026 Hugo Juarez. Licensed under the MIT License.
+*             See LICENSE file in the root of the repository for details.
+* ========================================================================= */
+#include "bmp390.h"
